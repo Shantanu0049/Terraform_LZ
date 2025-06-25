1b214dce94c6dfbd234fd6ac799d38a0e4955a4e
shantanu.gundge <shantanu.gundge@ATG-LAP-00178>
2025-06-25 15:28:10 +0530

    🔄 Update Terraform files from UI.py

--- main.tf
+++ main.tf
@@ -11,26 +11,6 @@ module "vm" {
   depends_on            = [module.networking, module.iam]
 }
 
-module "bigquery" {
-  source           = "./modules/bigquery"
-  project_id       = var.project_id
-  region           = var.region
-  dataset_id       = var.bq_dataset_id
-  table_ids        = var.bq_table_ids
-  subnet_self_link = module.networking.subnet_self_links[0]
-  depends_on       = [module.networking, module.iam]
-}
-
-module "dataproc" {
-  source           = "./modules/dataproc"
-  project_id       = var.project_id
-  region           = var.region
-  cluster_name     = var.dataproc_cluster_name
-  subnet_self_link = module.networking.subnet_self_links[1]
-  bucket_name = module.storage.bucket_names[0]
-  depends_on       = [module.networking, module.iam, module.storage]
-}
-
 module "networking" {
   source         = "./modules/networking"
   project_id     = var.project_id
@@ -59,5 +39,25 @@ module "storage" {
   depends_on                 = [module.networking, module.iam]
   
 
+}
+
+module "dataproc" {
+  source           = "./modules/dataproc"
+  project_id       = var.project_id
+  region           = var.region
+  cluster_name     = var.dataproc_cluster_name
+  subnet_self_link = module.networking.subnet_self_links[1]
+  bucket_name = module.storage.bucket_names[0]
+  depends_on       = [module.networking, module.iam, module.storage]
+}
+
+module "bigquery" {
+  source           = "./modules/bigquery"
+  project_id       = var.project_id
+  region           = var.region
+  dataset_id       = var.bq_dataset_id
+  table_ids        = var.bq_table_ids
+  subnet_self_link = module.networking.subnet_self_links[0]
+  depends_on       = [module.networking, module.iam]
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,15 +1,15 @@
 project_id             = "terraform-458410"
 region                 = "us-central1"
-network_name           = "landing-zone-vpc-testing1"
-subnet_names           = ["database-subnet1", "processing-subnet1", "storage-subnet1"]
+network_name           = "landing-zone-vpc-testing"
+subnet_names           = ["database-subnet", "processing-subnet", "storage-subnet"]
 subnet_cidrs           = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 subnet_regions         = ["us-central1", "us-central1", "us-central1"]
-custom_role_id         = "testing006543"
-role_permissions       = ["storage.objects.list", "storage.buckets.get"]
+custom_role_id         = "qwert"
+role_permissions       = ["storage.objects.get", "storage.buckets.get"]
 bucket_configs = [
 {
-      name                      = "atgeir-testing004555555555"
-      location                  = "us-central1"
+      name                      = ""
+      location                  = ""
       storage_class             = "STANDARD"
       versioning_enabled        = false
       uniform_bucket_level_access = false
@@ -18,6 +18,6 @@ bucket_configs = [
       force_destroy                = true
     }
 ]
-dataproc_cluster_name  = "landing-zone-cluster-testing1"
-bq_dataset_id          = "landing_zone_dataset_testing1"
-bq_table_ids           = ["raw_data1", "processed_data1", "final_output1"]
+dataproc_cluster_name  = "landing-zone-cluster-testing"
+bq_dataset_id          = "landing_zone_dataset_testing"
+bq_table_ids           = ["raw_data", "processed_data", "final_output"]
