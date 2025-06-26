6d0eaeaa7267ec7c4a7e08f08af41336c0b66912
shantanu.gundge <shantanu.gundge@ATG-LAP-00178>
2025-06-26 14:52:17 +0530

    Update TF via UI → feature-lz

--- main.tf
+++ main.tf
@@ -1,5 +1,36 @@
 # Auto-generated Terraform configuration
 
+module "vm" {
+  source                = "./modules/vm"
+  project_id            = var.project_id
+  zone                  = var.zone
+  instance_name         = var.instance_name
+  machine_type          = var.machine_type
+  service_account_email = module.iam.service_account_email
+  subnet                = module.networking.subnet_self_links[0]
+  depends_on            = [module.networking, module.iam]
+}
+
+module "bigquery" {
+  source           = "./modules/bigquery"
+  project_id       = var.project_id
+  region           = var.region
+  dataset_id       = var.bq_dataset_id
+  table_ids        = var.bq_table_ids
+  subnet_self_link = module.networking.subnet_self_links[0]
+  depends_on       = [module.networking, module.iam]
+}
+
+module "dataproc" {
+  source           = "./modules/dataproc"
+  project_id       = var.project_id
+  region           = var.region
+  cluster_name     = var.dataproc_cluster_name
+  subnet_self_link = module.networking.subnet_self_links[1]
+  bucket_name      = module.storage.bucket_names[0]
+  depends_on       = [module.networking, module.iam, module.storage]
+}
+
 module "networking" {
   source         = "./modules/networking"
   project_id     = var.project_id
@@ -20,44 +51,11 @@ module "iam" {
 }
 
 module "storage" {
-  source                     = "./modules/storage"
-  project_id                 = var.project_id
-  region                     = var.region
-  buckets                = var.bucket_configs
-  subnet_self_link           = module.networking.subnet_self_links[2]
-  depends_on                 = [module.networking, module.iam]
-  
-
-}
-
-module "dataproc" {
-  source           = "./modules/dataproc"
+  source           = "./modules/storage"
   project_id       = var.project_id
   region           = var.region
-  cluster_name     = var.dataproc_cluster_name
-  subnet_self_link = module.networking.subnet_self_links[1]
-  bucket_name = module.storage.bucket_names[0]
-  depends_on       = [module.networking, module.iam, module.storage]
-}
-
-module "bigquery" {
-  source           = "./modules/bigquery"
-  project_id       = var.project_id
-  region           = var.region
-  dataset_id       = var.bq_dataset_id
-  table_ids        = var.bq_table_ids
-  subnet_self_link = module.networking.subnet_self_links[0]
+  buckets          = var.bucket_configs
+  subnet_self_link = module.networking.subnet_self_links[2]
   depends_on       = [module.networking, module.iam]
-}
-
-module "vm" {
-  source                = "./modules/vm"
-  project_id            = var.project_id
-  zone                  = var.zone
-  instance_name         = var.instance_name
-  machine_type          = var.machine_type
-  service_account_email = module.iam.service_account_email
-  subnet                = module.networking.subnet_self_links[0]
-  depends_on            = [module.networking, module.iam]
 }
 
