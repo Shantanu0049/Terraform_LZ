120848b45db3d67fb88fb0d3926782459b448c73
shantanu.gundge <shantanu.gundge@ATG-LAP-00178>
2025-06-27 16:20:20 +0530

    Update TF → testing-lz

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
 # Auto-generated Terraform configuration
 
-module "bigquery" {
-  source           = "./modules/bigquery"
-  project_id       = var.project_id
-  region           = var.region
-  dataset_id       = var.bq_dataset_id
-  table_ids        = var.bq_table_ids
-  subnet_self_link = module.networking.subnet_self_links[0]
-  depends_on       = [module.networking, module.iam]
-}
-
 module "vm" {
   source                = "./modules/vm"
   project_id            = var.project_id
@@ -57,5 +47,15 @@ module "dataproc" {
   subnet_self_link = module.networking.subnet_self_links[1]
   bucket_name      = module.storage.bucket_names[0]
   depends_on       = [module.networking, module.iam, module.storage]
+}
+
+module "bigquery" {
+  source           = "./modules/bigquery"
+  project_id       = var.project_id
+  region           = var.region
+  dataset_id       = var.bq_dataset_id
+  table_ids        = var.bq_table_ids
+  subnet_self_link = module.networking.subnet_self_links[0]
+  depends_on       = [module.networking, module.iam]
 }
 
