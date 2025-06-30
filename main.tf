ac4ba79956304ef6e023eb87f689a270fc456ab7
shantanu.gundge <shantanu.gundge@ATG-LAP-00178>
2025-06-30 14:45:08 +0530

    Update TF → testing-lz

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
 # Auto-generated Terraform configuration
 
-module "vm" {
-  source                = "./modules/vm"
-  project_id            = var.project_id
-  zone                  = var.zone
-  instance_name         = var.instance_name
-  machine_type          = var.machine_type
-  service_account_email = module.iam.service_account_email
-  subnet                = module.networking.subnet_self_links[0]
-  depends_on            = [module.networking, module.iam]
-}
-
 module "networking" {
   source         = "./modules/networking"
   project_id     = var.project_id
@@ -21,15 +10,6 @@ module "networking" {
   subnet_regions = var.subnet_regions
 }
 
-module "iam" {
-  source           = "./modules/iam"
-  account_id       = "vm-service-account"
-  project_id       = var.project_id
-  custom_role_id   = var.custom_role_id
-  role_permissions = var.role_permissions
-  depends_on       = [module.networking]
-}
-
 module "storage" {
   source           = "./modules/storage"
   project_id       = var.project_id
@@ -37,6 +17,15 @@ module "storage" {
   buckets          = var.bucket_configs
   subnet_self_link = module.networking.subnet_self_links[2]
   depends_on       = [module.networking, module.iam]
+}
+
+module "iam" {
+  source           = "./modules/iam"
+  account_id       = "vm-service-account"
+  project_id       = var.project_id
+  custom_role_id   = var.custom_role_id
+  role_permissions = var.role_permissions
+  depends_on       = [module.networking]
 }
 
 module "dataproc" {
@@ -57,5 +46,16 @@ module "bigquery" {
   table_ids        = var.bq_table_ids
   subnet_self_link = module.networking.subnet_self_links[0]
   depends_on       = [module.networking, module.iam]
+}
+
+module "vm" {
+  source                = "./modules/vm"
+  project_id            = var.project_id
+  zone                  = var.zone
+  instance_name         = var.instance_name
+  machine_type          = var.machine_type
+  service_account_email = module.iam.service_account_email
+  subnet                = module.networking.subnet_self_links[0]
+  depends_on            = [module.networking, module.iam]
 }
 
