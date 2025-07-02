5baf8204ae167712c70bcae32c7f17ac7120410b
shantanu.gundge <shantanu.gundge@ATG-LAP-00178>
2025-07-02 17:04:45 +0530

    Update TF → testing-lz

--- main.tf
+++ main.tf
@@ -1,22 +1,14 @@
 # Auto-generated Terraform configuration
 
-module "networking" {
-  source         = "./modules/networking"
-  project_id     = var.project_id
-  region         = var.region
-  network_name   = var.network_name
-  subnet_names   = var.subnet_names
-  subnet_cidrs   = var.subnet_cidrs
-  subnet_regions = var.subnet_regions
-}
-
-module "storage" {
-  source           = "./modules/storage"
-  project_id       = var.project_id
-  region           = var.region
-  buckets          = var.bucket_configs
-  subnet_self_link = module.networking.subnet_self_links[2]
-  depends_on       = [module.networking, module.iam]
+module "vm" {
+  source                = "./modules/vm"
+  project_id            = var.project_id
+  zone                  = var.zone
+  instance_name         = var.instance_name
+  machine_type          = var.machine_type
+  service_account_email = module.iam.service_account_email
+  subnet                = module.networking.subnet_self_links[0]
+  depends_on            = [module.networking, module.iam]
 }
 
 module "iam" {
@@ -26,6 +18,15 @@ module "iam" {
   custom_role_id   = var.custom_role_id
   role_permissions = var.role_permissions
   depends_on       = [module.networking]
+}
+
+module "storage" {
+  source           = "./modules/storage"
+  project_id       = var.project_id
+  region           = var.region
+  buckets          = var.bucket_configs
+  subnet_self_link = module.networking.subnet_self_links[2]
+  depends_on       = [module.networking, module.iam]
 }
 
 module "dataproc" {
@@ -48,14 +49,13 @@ module "bigquery" {
   depends_on       = [module.networking, module.iam]
 }
 
-module "vm" {
-  source                = "./modules/vm"
-  project_id            = var.project_id
-  zone                  = var.zone
-  instance_name         = var.instance_name
-  machine_type          = var.machine_type
-  service_account_email = module.iam.service_account_email
-  subnet                = module.networking.subnet_self_links[0]
-  depends_on            = [module.networking, module.iam]
+module "networking" {
+  source         = "./modules/networking"
+  project_id     = var.project_id
+  region         = var.region
+  network_name   = var.network_name
+  subnet_names   = var.subnet_names
+  subnet_cidrs   = var.subnet_cidrs
+  subnet_regions = var.subnet_regions
 }
 
