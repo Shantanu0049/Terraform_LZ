59a054a66a19cebac8ac54e01a9303394b1913a5
shantanu.gundge <shantanu.gundge@ATG-LAP-00178>
2025-07-03 14:44:47 +0530

    Update TF → testing-lz

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 # Auto-generated Terraform configuration
 
+module "bigquery" {
+  source           = "./modules/bigquery"
+  project_id       = var.project_id
+  region           = var.region
+  dataset_id       = var.bq_dataset_id
+  table_ids        = var.bq_table_ids
+  subnet_self_link = module.networking.subnet_self_links[0]
+  depends_on       = [module.networking, module.iam]
+}
+
 module "vm" {
   source                = "./modules/vm"
   project_id            = var.project_id
@@ -9,6 +19,16 @@ module "vm" {
   service_account_email = module.iam.service_account_email
   subnet                = module.networking.subnet_self_links[0]
   depends_on            = [module.networking, module.iam]
+}
+
+module "dataproc" {
+  source           = "./modules/dataproc"
+  project_id       = var.project_id
+  region           = var.region
+  cluster_name     = var.dataproc_cluster_name
+  subnet_self_link = module.networking.subnet_self_links[1]
+  bucket_name      = module.storage.bucket_names[0]
+  depends_on       = [module.networking, module.iam, module.storage]
 }
 
 module "networking" {
@@ -36,26 +56,6 @@ module "storage" {
   region           = var.region
   buckets          = var.bucket_configs
   subnet_self_link = module.networking.subnet_self_links[2]
-  depends_on       = [module.networking, module.iam]
-}
-
-module "dataproc" {
-  source           = "./modules/dataproc"
-  project_id       = var.project_id
-  region           = var.region
-  cluster_name     = var.dataproc_cluster_name
-  subnet_self_link = module.networking.subnet_self_links[1]
-  bucket_name      = module.storage.bucket_names[0]
-  depends_on       = [module.networking, module.iam, module.storage]
-}
-
-module "bigquery" {
-  source           = "./modules/bigquery"
-  project_id       = var.project_id
-  region           = var.region
-  dataset_id       = var.bq_dataset_id
-  table_ids        = var.bq_table_ids
-  subnet_self_link = module.networking.subnet_self_links[0]
   depends_on       = [module.networking, module.iam]
 }
 
